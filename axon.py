a9ced218f12a34033fbeb9fb0b13a254cb893b7c
Kendrick Shaw <kms15@case.edu>
2014-11-30 23:47:57 +0000

    break the axon into sections to allow different conductances

--- axon.py
+++ axon.py
@@ -1,20 +1,46 @@
 from neuron import h
 import pylab # (used for plotting)
 
-# create the axon
-axon = h.Section()
-axon.L = 1000.
-axon.nseg = 100
-axon.diam = 1.
-axon.insert('hh')
-axon.Ra = 100.
-axon.cm = 1.
-#for seg in axon:
-#    seg.Ra = 100
-#    seg.cm = 1
+# NEURON defines a coordinate system along the length of a segment from 0 to 1;
+# for consistency we will define 0 as the left side of the axon and 1 as the
+# right side.
+left_side = 0.
+right_side = 1.
 
-stim_position = 0.
-stim = h.IClamp(stim_position, axon)
+# create the axon
+def create_axon(
+        num_sections = 1000, # number of compartments along the axon's length
+        length = 1000.,      # total length of axon (um)
+        diam = 1.,           # axonal diameter (um)
+        ):
+
+    # create the sections
+    sections = [h.Section() for i in range(num_sections)]
+
+    # initialize the sections
+    for sec in sections:
+        # set the geometry
+        sec.L = length/num_sections
+        sec.diam = diam
+
+        # set the passive properties
+        sec.cm = 1.
+        sec.Ra = 100.
+
+        # insert the active channels
+        sec.insert('hh')
+
+    # connect the sections end to end into a chain, connecting the right
+    # side of each segment to the left side of the next
+    for i in range(num_sections - 1):
+        sections[i].connect(sections[i+1], left_side, right_side)
+
+    return sections
+
+axon = create_axon()
+
+# insert the stimulus
+stim = h.IClamp(left_side, axon[0])
 stim.amp = 10.
 stim.delay = 5.
 stim.dur = 5.
@@ -23,7 +49,7 @@ stim.dur = 5.
 t = h.Vector()
 t.record(h._ref_t)
 v = h.Vector()
-v.record(axon(1.)._ref_v)
+v.record(axon[-1](right_side)._ref_v)
 
 # run the simulation
 h.load_file("stdrun.hoc")
@@ -40,5 +66,5 @@ pylab.show()
 
 # pdf and png export seem to be broken in NEURON - see comment here
 # http://www.neuron.yale.edu/phpbb/viewtopic.php?f=2&t=2097 -
-# thus we export the figure as a pdf
+# thus we export the figure as an eps
 pylab.savefig("plot.eps") #, bbox_inches='tight')

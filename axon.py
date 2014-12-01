f73cfe3e264b34a126ec621881280dd67e59e35d
Kendrick Shaw <kms15@case.edu>
2014-12-01 14:28:21 +0000

    demo of local heat block

--- axon.py
+++ axon.py
@@ -3,17 +3,18 @@ import pylab # (used for plotting)
 
 # A NEURON model of an unmylenated axon
 class Axon:
-    # NEURON defines a coordinate system along the length of a segment from 0 to 1;
+    # NEURON defines a coordinate system along the length of a section from 0 to 1;
     # for consistency we will define 0 as the left side of the axon and 1 as the
     # right side.
     left_side = 0.
     right_side = 1.
     middle = 0.5
 
-    # create the axon
+
+    # Create the axon
     def __init__(self,
             num_sections = 1000, # number of compartments along the axon's length
-            length = 5000.,      # total length of axon (um)
+            length = 1000.,      # total length of axon (um)
             diam = 1.,           # axonal diameter (um)
             ):
 
@@ -46,31 +47,86 @@ class Axon:
             self.sections[i].connect(self.sections[i+1], Axon.left_side,
                     Axon.right_side)
 
-    # get the section a given fraction of the length along the axon
-    def section_at_f(self, f):
+
+    # Get the index of the section at the given length along the axon
+    def section_id_at_f(self, f):
+        # parameter check
+        if f < 0.:
+            raise ValueError("position beyond left end of axon")
+        if f > 1.:
+            raise ValueError("position beyond right end of axon")
+
         if (f == 1):
             # corner case: normally a section extends from the left boundary of
             # the section up to, but *not* including, the right boundary (which
             # is the left boundary of the next section).  The rightmost section
             # needs to include its right boundary, however.
-            return self.sections[-1]
+            return len(self.sections) - 1
         else:
-            return self.sections[int(f * len(self.sections))]
+            return int(f * len(self.sections));
 
-    # get the section containing the given position along the axon
+
+    # Get the section a given fraction of the length along the axon
+    def section_at_f(self, f):
+        return self.sections[self.section_id_at_f(f)]
+
+
+    # Get the section containing the given position along the axon
     def section_at_x(self, x):
         return self.section_at_f(x / self.length)
 
-    # insert a simple current at the given position
+
+    # Apply a function to all the sections that are part of a given range
+    # of positions along the axon.  The end sections of the range are
+    # included, even though they may be only partially within the range.
+    def apply_to_sections(self,
+            func,         # function to apply, of the form func(sec, x_center)
+            x_start = 0., # start of the range of positions, measured from left
+            x_end = None  # end of the range; if None everything to the right
+                          # x_start will be included.
+            ):
+        # parameter checking and cleanup
+        if x_end == None:
+            x_end = self.length
+        if x_end < x_start:
+            raise ValueError("x_end must be to the right of x_start")
+
+        for i in range(self.section_id_at_f(x_start / self.length),
+                self.section_id_at_f(x_end / self.length) + 1):
+            func(self.sections[i],
+                    (i + Axon.middle) * self.length / len(self.sections))
+
+
+    # Insert a simple current at the given position
     def insert_stim(self, x=0.):
         stim = h.IClamp(Axon.middle, self.section_at_x(x))
-        stim.amp = 1.  # nA
-        stim.delay = 1. # ms
-        stim.dur = 1.   # ms
+        stim.amp = 1.    # nA
+        stim.delay = 0.1 # ms
+        stim.dur = 1.    # ms
 
         # NOTE: NEURON will remove the stimulus as soon as it's no longer
         # reachable from python code, so we need to store it.
         self.stimuli.append(stim)
+
+
+    # Set the temperature of a range of sections
+    def set_temp(self,
+            temp,       # a temperature (Celsius) or a function of the form
+                        # g(x) that returns the temperature at position x.
+            x_start=0,  # start of the range of positions (0 == left end)
+            x_end=None  # end of the range of positions (None == right end)
+            ):
+        # if temperature is not a function, turn it into one.
+        if hasattr(temp, '__call__'):
+            temp_at_x = temp
+        else:
+            temp_at_x = lambda x: temp
+
+        def set_section_temp(sec, x):
+            sec.localtemp_fhm1 = temp_at_x(x)
+
+        self.apply_to_sections(set_section_temp, x_start, x_end);
+
 
 
 # if we're running this code directly (vs. importing it as a library),
@@ -78,22 +134,27 @@ class Axon:
 if __name__ == "__main__":
     axon = Axon()
     axon.insert_stim()
+    axon.set_temp(54.49, 300, 600)
 
     # set up recording vectors
     t = h.Vector()
     t.record(h._ref_t)
 
-    num_v_traces = 10
+    num_v_traces = 20
     v_traces = []
     for i in range(num_v_traces):
         v = h.Vector()
-        v.record(axon.section_at_f(i/(num_v_traces - 1.))
+        v.record(axon.section_at_f(
+            # record at num_v_traces points along the axon, equally spaced
+            # from eachother and from the end points (since we don't care
+            # about things like the impedance mismatch at the ends)
+            (i + 1) * 1.0 / (num_v_traces + 1))
             (Axon.middle)._ref_v)
         v_traces.append(v)
 
     # initialize the simulation
-    h.dt = 0.025 # integration time step, in ms
-    tstop = 19.9   # duration of integration
+    h.dt = 0.005 # integration time step, in ms
+    tstop = 2.99 # duration of integration
     v_init = -65 # initial membrane potential, in mV
     h.finitialize(v_init)
     h.fcurrent()
